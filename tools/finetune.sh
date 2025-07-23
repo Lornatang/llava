3d55493dda60f8857119fe3d75dbe6a47ddeadb5
Lornatang <liuchangyu1111@gmail.com>
2025-07-23 23:47:35 +0800

    Update finetune.sh to use the correct training script path

--- tools/finetune.sh
+++ tools/finetune.sh
@@ -1,4 +1,4 @@
-deepspeed llava/train/train_mem.py \
+deepspeed ./tools/train.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path ./results/pretrained_models/lmsys/vicuna-13b-v1.5 \
     --version v1 \
