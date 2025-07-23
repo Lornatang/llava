532b3b29c54aed20a7291ac906c802004b2fab4e
Lornatang <liuchangyu1111@gmail.com>
2025-07-23 23:43:12 +0800

    Update pretrain.sh output directory and add finetune.sh for model fine-tuning

--- tools/finetune.sh
+++ tools/finetune.sh
@@ -1,32 +1,36 @@
-deepspeed ./tools/train.py \
-    --deepspeed ./tools/zero3.json \
+deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
     --model_name_or_path ./results/pretrained_models/lmsys/vicuna-13b-v1.5 \
-    --version plain \
-    --data_path ./datasets/llava_pretrain/blip_laion_cc_sbu_558k.json \
-    --image_folder ./datasets/llava_pretrain/images \
+    --version v1 \
+    --data_path ./datasets/llava_finetune/llava_v1_5_mix665k.json \
+    --image_folder ./datasets/llava_finetune \
     --vision_tower ./results/pretrained_models/openai/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter ./results/llava-vicuna_13b_v1.5-clip_vit_large_patch14_336-pretrain-blip_laion_cc_sbu_558k/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
-    --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./results/llava-vicuna_13b_v1.5-clip_vit_large_patch14_336-pretrain-blip_laion_cc_sbu_558k \
+    --output_dir ./results/./results/llava-vicuna_13b_v1.5-clip_vit_large_patch14_336-finetune-llava_v1_5_mix665k \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 32 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 24000 \
+    --save_steps 50000 \
     --save_total_limit 1 \
-    --learning_rate 1e-3 \
+    --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 False \
+    --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
+    --report_to wandbrue \
     --report_to wandb
--- tools/train.py
+++ tools/train.py
@@ -977,8 +977,7 @@ def train(attn_implementation=None):
             model.save_pretrained(training_args.output_dir, state_dict=state_dict)
             torch.save(non_lora_state_dict, os.path.join(training_args.output_dir, "non_lora_trainables.bin"))
     else:
-        safe_save_model_for_hf_trainer(trainer=trainer,
-                                       output_dir=training_args.output_dir)
+        safe_save_model_for_hf_trainer(trainer=trainer, output_dir=training_args.output_dir)
 
 
 if __name__ == "__main__":
