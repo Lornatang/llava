9836949a5bee750e7b67217ec2bbb3f81cc4d588
Lornatang <liuchangyu1111@gmail.com>
2025-07-26 10:37:38 +0800

    Refactor imports in __init__.py to streamline trainer module usage

--- llava/engine/__init__.py
+++ llava/engine/__init__.py
@@ -11,7 +11,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from .trainer import (
-    LengthGroupedSampler, Trainer, get_mm_adapter_state_maybe_zero_3, get_mm_length_grouped_indices, maybe_zero_3,
-    split_to_even_chunks
-)
+from .trainer import LengthGroupedSampler, LLaVATrainer
