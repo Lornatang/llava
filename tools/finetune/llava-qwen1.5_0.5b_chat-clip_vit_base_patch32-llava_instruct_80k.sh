2828244f23804c7a368605ab27446b94bc3bd270
Lornatang <liuchangyu1111@gmail.com>
2025-08-08 17:27:26 +0800

    Update shell scripts for Qwen1.5 training and rename existing scripts

---
+++ tools/finetune/llava-qwen1.5_0.5b_chat-clip_vit_base_patch32-llava_instruct_80k.sh
@@ -0,0 +1,51 @@
+export OMP_NUM_THREADS=8
+export NCCL_IB_DISABLE=0
+export NCCL_IB_GID_INDEX=3
+export NCCL_SOCKET_IFNAME=eno1
+export NCCL_DEBUG=INFO
+export ACCELERATE_CPU_AFFINITY=1
+export TOKENIZERS_PARALLELISM=false
+
+torchrun --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 --master_port=10000 \
+    ./tools/train.py \
+    --deepspeed ./tools/zero3.json \
+    --model_name_or_path "./results/pretrained_models/Qwen/Qwen1.5-0.5B-Chat" \
+    --version "qwen1_5" \
+    --data_path "./datasets/llava_finetune/llava_instruct_80k.json" \
+    --image_folder "./datasets/llava_finetune/coco/train2017" \
+    --mm_tunable_parts="mm_vision_tower,mm_mlp_adapter,mm_language_model" \
+    --mm_vision_tower_lr=2e-6 \
+    --vision_tower "./results/pretrained_models/openai/clip-vit-base-patch32" \
+    --mm_projector_type "mlp2x_gelu" \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --group_by_modality_length True \
+    --image_aspect_ratio "anyres_max_9" \
+    --image_grid_pinpoints  "(1x1),...,(6x6)" \
+    --mm_patch_merge_type "spatial_unpad" \
+    --output_dir "./results/finetune/llava-qwen1.5_0.5b_chat-clip_vit_base_patch32-llava_instruct_80k" \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 2 \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --bf16 True \
+    --tf32 True \
+    --model_max_length 32768 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to "wandb" \
+    --run_name "llava-qwen1.5_0.5b_chat-clip_vit_base_patch32-llava_instruct_80k" \
+    --torch_compile True \
+    --torch_compile_backend "eager" \
+    --dataloader_drop_last True \
+    --frames_upbound 32
