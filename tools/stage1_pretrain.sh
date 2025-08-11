541fa052d1b5471849f5ebd1f334b07341047dd4
Lornatang <liuchangyu1111@gmail.com>
2025-08-11 10:04:26 +0800

    Add stage1 pretraining script with environment and training configurations

---
+++ tools/stage1_pretrain.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Environment Configuration.
+export OMP_NUM_THREADS=8
+export ACCELERATE_CPU_AFFINITY=1
+export TOKENIZERS_PARALLELISM=false
+
+# Distributed Configuration.
+export NCCL_IB_DISABLE=0
+export NCCL_IB_GID_INDEX=3
+export NCCL_SOCKET_IFNAME=eno1
+export NCCL_DEBUG=INFO
+NPROC_PER_NODE=$(nvidia-smi --list-gpus | wc -l)
+MASTER_ADDR="127.0.0.1"
+MASTER_PORT=$(shuf -i 10000-19999 -n 1)
+
+# Data Configuration.
+DATA_PATH="./datasets/llava_pretrain/blip_laion_cc_sbu_558k.json"
+IMAGE_FOLDER="./datasets/llava_pretrain/images"
+
+# Train Configuration.
+MODEL_PATH="./results/pretrained_models/lmsys/vicuna-13b-v1.5"
+VISION_MODEL_PATH="./results/pretrained_models/openai/clip-vit-large-patch14-336"
+RUN_NAME="llava-vicuna_13b_v1.5-clip_vit_large_patch14_336-blip_laion_cc_sbu_558k"
+ATTN_IMPLEMENTATION="flash_attention_2"
+DEEPSPEED_CONFIG="./tools/zero3.json"
+
+# Training Hyperparameters
+torchrun --nproc_per_node=${NPROC_PER_NODE} \
+         --master_addr=${MASTER_ADDR} \
+         --master_port=${MASTER_PORT} \
+         ./tools/train.py \
+         --model_name_or_path ${MODEL_PATH} \
+         --version "llava_plain" \
+         --data_path ${DATA_PATH} \
+         --image_folder ${IMAGE_FOLDER} \
+         --vision_tower ${VISION_MODEL_PATH} \
+         --mm_tunable_parts "mm_mlp_adapter" \
+         --mm_vision_select_layer -2 \
+         --mm_projector_type "mlp2x_gelu" \
+         --mm_use_im_start_end False \
+         --mm_use_im_patch_token False \
+         --output_dir "./results/pretrain/${RUN_NAME}" \
+         --num_train_epochs 1 \
+         --per_device_train_batch_size 16 \
+         --gradient_accumulation_steps 1 \
+         --dataloader_num_workers 16 \
+         --learning_rate 1e-3 \
+         --lr_scheduler_type "cosine" \
+         --weight_decay 0. \
+         --warmup_ratio 0.03 \
+         --save_strategy "steps" \
+         --save_steps 1000 \
+         --save_total_limit 1 \
+         --logging_steps 1 \
+         --bf16 True \
+         --tf32 True \
+         --model_max_length 8192 \
+         --gradient_checkpointing True \
+         --lazy_preprocess True \
+         --report_to "wandb" \
+         --run_name ${RUN_NAME} \
+         --attn_implementation ${ATTN_IMPLEMENTATION} \
+         --deepspeed ${DEEPSPEED_CONFIG} \
